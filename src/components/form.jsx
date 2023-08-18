579bce8252207d4e4efd5e3f9861a428a2faa637
MHNazary <mhnazary2020@gmail.com>
2023-08-18 12:41:21 +0430

    README.md updated

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Form = () => (
+function Form() {
   <div>
-    <h3 className="book-title">ADD NEW BOOK</h3>
+    <h3 className="form-book-title">Add New Book</h3>
     <form>
       <input type="text" placeholder="Book Title" />
       <input type="text" placeholder="Author" />
-      <button type="button" className="button" id="addBttn">
+      <button type="button" className="button" id="addBtn">
         ADD BOOK
       </button>
     </form>
-  </div>
-);
+  </div>;
+}
 
 export default Form;
