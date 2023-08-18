d64e81580b0aa0b04a1b8f524b3409edd2f6281d
MHNazary <mhnazary2020@gmail.com>
2023-08-18 12:07:48 +0430

    Use router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BookList from './components/bookList';
+import Catagories from './catagories/catagories';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/catagories" element={<Catagories />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
