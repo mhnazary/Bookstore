6eac432606ba2a3ae96301a8207ffd9c46fb690b
MHNazary <mhnazary2020@gmail.com>
2023-08-18 13:27:52 +0430

    Fix form.jsx error

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 function Form() {
-  <div>
-    <h3 className="form-book-title">Add New Book</h3>
-    <form>
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Author" />
-      <button type="button" className="button" id="addBtn">
-        ADD BOOK
-      </button>
-    </form>
-  </div>;
+  return (
+    <div>
+      <h3 className="form-book-title">Add New Book</h3>
+      <form>
+        <input type="text" placeholder="Book Title" />
+        <input type="text" placeholder="Author" />
+        <button type="button" className="button" id="addBtn">
+          ADD BOOK
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
