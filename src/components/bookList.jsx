25028de29ccfb0f9c8b69594e4886f631e3d5f32
MHNazary <mhnazary2020@gmail.com>
2023-08-23 21:10:52 +0430

    Update booklistSlice by adding API

--- src/components/bookList.jsx
+++ src/components/bookList.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getBooks } from '../redux/books/booksSlice';
 import Book from './books';
 
 const BookList = () => {
-  const { books } = useSelector((state) => state.books);
+  const BK = useSelector((state) => Object.values(state.BK).flatMap((bookArray) => bookArray));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  if (BK.length === 0) {
+    return <p>Add some books</p>;
+  }
 
   return (
-    <div>
-      {books.map((book) => (
-        <Book
-          key={book.item_id}
-          id={book.item_id}
-          title={book.title}
-          author={book.author}
-        />
-      ))}
-    </div>
+    <>
+      <div>
+        {BK.map((book) => (
+          <Book
+            key={book.item_id}
+            id={book.item_id}
+            title={book.title}
+            author={book.author}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,40 +1,70 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-};
+const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/3M9u7ePlYhtNH1JJSthG/books';
+
+export const getBooks = createAsyncThunk(
+  'books/getBooks',
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get(API);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Fetch error... try again');
+    }
+  },
+);
+// post
+export const postBook = createAsyncThunk('books/postBook', async (data) => {
+  try {
+    const response = await axios.post(API, data);
+    return {
+      message: response.data,
+      data,
+    };
+  } catch (error) {
+    return error;
+  }
+});
+export const deleteBook = createAsyncThunk('books/deleteBook', async (id) => {
+  try {
+    await axios.delete(`${API}/${id}`);
+    return id;
+  } catch (error) {
+    return error;
+  }
+});
 
 const booksSlice = createSlice({
-  name: 'books', // Corrected the name to 'books'
-  initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.books.push(action.payload);
-    },
-    removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.item_id !== action.payload);
-    },
+  name: 'book',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.fulfilled, (state, action) => {
+        const booksObj = action.payload;
+        const bookIdArray = Object.keys(booksObj);
+        const books = bookIdArray.map((bookId) => ({
+          item_id: bookId,
+          title: booksObj[bookId][0].title,
+          author: booksObj[bookId][0].author,
+          category: booksObj[bookId][0].category,
+        }));
+        return { ...state, books };
+      })
+      .addCase(postBook.fulfilled, (state, action) => {
+        const IsFulfilled = state;
+        IsFulfilled.books = [action.payload.data, ...IsFulfilled.books];
+      })
+      .addCase(deleteBook.fulfilled, (state, action) => {
+        const filteredBooks = state.books.filter(
+          (book) => book.item_id !== action.payload,
+        );
+        return { ...state, books: filteredBooks };
+      });
   },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
+
 export default booksSlice.reducer;
