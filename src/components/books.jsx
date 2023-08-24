407cd34b312da7510e7c5d610e94ad9c5c21d4e7
MHNazary <mhnazary2020@gmail.com>
2023-08-25 01:20:43 +0430

    Added style for navbar and book

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../redux/books/booksSlice';
-import '../index.css';
+import logo from './Logo.png';
 
 const Book = ({ id, title, author }) => {
+  const [change, setChange] = useState(15);
   const dispatch = useDispatch();
-
   const handleRemoveBook = () => {
     dispatch(deleteBook(id));
   };
+  function handleChange() {
+    setChange(change + 1);
+  }
 
   return (
-    <section className="book">
-      <div>
-        <h3 className="book-title">{title}</h3>
-        <p className="book-author">{author}</p>
+    <section className="book flex flex-row justify-between">
+      <div className="book-right-part w-1/2 flex flex-col justify-evenly">
+        <div>
+          <h3 className="book-title font-serif font-bold text-xl mb-1">{title}</h3>
+          <p className="book-author text-sm font-serif text-blue font-extralight">{author}</p>
+        </div>
+        <div className="flex justify-evenly w-1/2 items-center text-blue font-serif text-base mt-1">
+          <a href="###" onClick={handleRemoveBook}>
+            commens
+          </a>
+          <a href="###" onClick={handleRemoveBook}>
+            remove
+          </a>
+          <a href="###" onClick={handleRemoveBook}>
+            edit
+          </a>
+        </div>
       </div>
-      <button
-        type="button"
-        className="button border border-black rounded-sm flex justify-center items-start pb-1 mt-2 text-blue"
-        id="deleteBttn"
-        onClick={handleRemoveBook}
-      >
-        Remove
-      </button>
+      <div className="book-left-part flex justify-start w-1/2">
+        <div className="w-1/2 p-2">
+          <img src={logo} alt="percent" />
+        </div>
+        <div className="w-1/2 mt-4">
+          <h1 className="font-serif text-gray-400">Current Chapter</h1>
+          <h1 className="font-serif mt-2">
+            Chapter
+            <span> </span>
+            {change}
+          </h1>
+          <button
+            className="bg-blue px-4 py-1 text-white rounded-sm mt-3 text-base font-light font-serif"
+            type="button"
+            onClick={handleChange}
+          >
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        blue: '#009aed',
+        blue: '#0290ff',
+        darkwhite: '#f5f6fa',
       },
     },
   },
